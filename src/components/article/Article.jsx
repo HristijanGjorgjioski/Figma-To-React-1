f141f03e7ae11738c92f8d17c7797ef1517266db
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-18 10:02:28 -0500

    Creating Blog section and Article helper component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import './article.css'
 
-const Article = () => {
+const Article = ({ imgUrl }) => {
     return (
-        <div>
-            Article
+        <div className="gpt3__blog-container_article">
+            <div className="gpt3__blog-container_article-image">
+                <img src={imgUrl} alt="blog img" />
+            </div>
         </div>
     )
 }
